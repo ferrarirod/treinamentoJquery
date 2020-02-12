5583636d89ae0f4eb155489dd0ea4a80e8fb2ab1
ferrarirod <ferrari_rod@hotmail.com>
2020-02-12 09:44:00 -0300

    demonstracao de css feita

---
+++ public/js/cssEanimate.js
@@ -0,0 +1,6 @@
+$('#cssBtn').click(function(){
+    var r = Math.floor(Math.random() * 256);
+    var g = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random() * 256);
+    $('#cssText').css('color','rgb('+ r + ',' + g + ',' + b +')');
+});
