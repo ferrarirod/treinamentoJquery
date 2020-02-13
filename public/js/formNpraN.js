7970672961d7eb378b454ce7bbb8bfaaba8f2800
Rodrigo Ferrari <ferrari_rod@hotmail.com>
2020-02-13 10:52:02 -0300

    ajuste nas funcoes ja prontas

--- public/js/formNpraN.js
+++ public/js/formNpraN.js
@@ -4,14 +4,14 @@ function addModel(select)
 
     if($(select).val() == 1)
     {
-        $(select).closest('.formRow').find('.customForm').html($('.formModels .foto').contents().clone());
+        $(select).closest('.formRow').find('.customForm').html($('.formModels .foto').clone());
     }
     else if ($(select).val() == 2)
     {
-        $(select).closest('.formRow').find('.customForm').html($('.formModels .adesivo').contents().clone());
+        $(select).closest('.formRow').find('.customForm').html($('.formModels .adesivo').clone());
     }
     else if ($(select).val() == 3)
     {
-        $(select).closest('.formRow').find('.customForm').html($('.formModels .banner').contents().clone());
+        $(select).closest('.formRow').find('.customForm').html($('.formModels .banner').clone());
     }
 }
